2d65ce1b499e6a2782b21b2b42680cb26ccbd28e
cristiangsmiranda <san24063@byui.edu>
2025-04-14 16:22:49 -0300

    database updated

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340cr3;
+    OWNER TO cse340cr4;
 
 
 CREATE TABLE public.classification (
